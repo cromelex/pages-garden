a8651368f64caddf82cf5df8cdf03adf8c516ae1
cromelex <dan@dansgarden.eu>
2025-03-24 12:03:00 +0000

    fixed compatibility with Quartz 4.5.0
    rolled back some Explorer changes and custom.css to make this work properly with the latest version of quartz

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,53 +18,33 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.ConditionalRender({
+      component: Component.Breadcrumbs(),
+      condition: (page) => page.fileData.slug !== "index",
+    }),
+    Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
-    Component.Darkmode(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+      ],
+    }),
     Component.Explorer({
       title: "Home", // title of the explorer component
-      folderClickBehavior: "link",
-      folderDefaultState: "collapsed",
       filterFn: (node) => {
-        // set containing names of everything you want to filter out
-        const omit = new Set(["contact"])
-        return !omit.has(node.name.toLowerCase())
+        // exclude files with the tag "explorerexclude"
+        return node.data.tags.includes("explorerexclude") !== true
       },
-      sortFn: (a, b) => {
-        const nameOrderMap: Record<string, number> = {
-          "economics": 100,
-          "fatherhood": 200,
-          //"fatherhood/paternity-leave": 201,
-          //"fatherhood/fatherhood-pt2": 202,
-          //"fatherhood/fatherhood-pt1": 203,
-          "technology": 300,
-          "projects": 400,
-          "misc": 700,
-          "about": 999,
-        }
-
-        let orderA = 0
-        let orderB = 0
-
-        if (a.file && a.file.slug) {
-          orderA = nameOrderMap[a.file.slug] || 0
-        } else if (a.name) {
-          orderA = nameOrderMap[a.name] || 0
-        }
-
-        if (b.file && b.file.slug) {
-          orderB = nameOrderMap[b.file.slug] || 0
-        } else if (b.name) {
-          orderB = nameOrderMap[b.name] || 0
-        }
-
-        return orderA - orderB
-      },
-    }),
+    })
   ],
   afterBody: [
     Component.ReplyByEmail(),
@@ -78,51 +58,26 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
-    Component.Darkmode(),
     Component.MobileOnly(Component.Spacer()),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+      ],
+    }),
     Component.Explorer({
       title: "Home", // title of the explorer component
-      folderClickBehavior: "link",
-      folderDefaultState: "collapsed",
       filterFn: (node) => {
-        // set containing names of everything you want to filter out
-        const omit = new Set(["contact"])
-        return !omit.has(node.name.toLowerCase())
+        // exclude files with the tag "explorerexclude"
+        return node.data.tags.includes("explorerexclude") !== true
       },
-      sortFn: (a, b) => {
-        const nameOrderMap: Record<string, number> = {
-          "economics": 100,
-          "fatherhood": 200,
-          //"fatherhood/paternity-leave": 201,
-          //"fatherhood/fatherhood-pt2": 202,
-          //"fatherhood/fatherhood-pt1": 203,
-          "technology": 300,
-          "projects": 400,
-          "misc": 700,
-          "about": 999,
-        }
-
-        let orderA = 0
-        let orderB = 0
-
-        if (a.file && a.file.slug) {
-          orderA = nameOrderMap[a.file.slug] || 0
-        } else if (a.name) {
-          orderA = nameOrderMap[a.name] || 0
-        }
-
-        if (b.file && b.file.slug) {
-          orderB = nameOrderMap[b.file.slug] || 0
-        } else if (b.name) {
-          orderB = nameOrderMap[b.name] || 0
-        }
-
-        return orderA - orderB
-      },
-    }),
+    })
   ],
   right: [
     Component.Search(),
